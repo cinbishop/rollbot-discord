2db4306aa0e09f0716225a3573489162c307ce75
Fox <Fox@Jacobs-MacBook-Pro.local>
2016-02-25 22:11:26 -0500

    mmm

--- rollbot.js
+++ rollbot.js
@@ -3,8 +3,8 @@ var request = require('request');
 module.exports = function (req, res, next) {
   // default roll is 1d20
   var matches;
-  var defaultmod;
   var weapons;
+  var rollNote = "";
   var times = 1;
   var die = 20;
   var modifier = "+";
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
   var botPayload = {};
   var missEmojiArray = [":hatched_chick:",":poop:",":baby_chick:",":laughing:",":frowning:",":thumbsdown:"];
   var hitEmojiArray = [":bangbang:",":clap:",":rage:",":hammer:",":bomb:",":skull:"];
-  var critArray = [" CRIT!"," AWH YEAH BIG CRITS!"," CRITTY DITTY DO!"," MMM SEXY CRIT TIMES!"," CRITATTACK!"," M-M-M-MONSTER KILL!", " SUCH CRIT. MUCH DAMAGE.", " GOING... GOING... GONE!", " YAY BIG NUMBERS!", " NICE CRIT, SEXY.", " YOU DONE GOOD, KID", " CRITALCULAR!", " DOINK!","NICE ONE, BRUVA!"]
+  var critArray = [" CRIT!"," AWH YEAH BIG CRITS!"," CRITTY DITTY DO!"," MMM SEXY CRIT TIMES!"," CRITATTACK!"," M-M-M-MONSTER KILL!", " SUCH CRIT. MUCH DAMAGE.", " GOING... GOING... GONE!", " YAY BIG NUMBERS!", " NICE CRIT, SEXY.", " YOU DONE GOOD, KID", " CRITALCULAR!", " DOINK!"," NICE ONE, BRUVA!"]
   var missArray = [" YOU SUCK B!"," YOU JUST HIT YOURSELF!"," SLICE! THERE GOES YA PENIS!"," CRITICAL MISS, NUBCAKES!"," WIGGIDTY WAM WAM WOZZLE YA MISSED!"," BABBY'S FIRST SWORD SWING!" ," IS YOUR NAME STEVE FISHER? MAN YOU SUCK.", " LOLOLOLOLOLOL REZ INCOMING NUB", " THAT'S RARELY GOOD", " YA DONE GOOFED", " BIFFED IT", " BIFFED IT HARD", " WELL AT LEAST YOUR PARENTS STILL LOVE YOU", " THIS IS WHY WE CAN'T HAVE NICE THINGS", " I CAN'T BELIEVE YOU'VE DONE THIS", " GOOD JOB, [BLIND CELEBRITY NAME HERE]"]
   var nameArray = ["ZIMZAMTHEROLLYMAN","ROLLBOT","TRANSFORMANDROLLOUTBOT","BOLLROT","ROLLYPOLLYOLLYROLLBOT","ROLLSMAN5000","CRITOMATIC","MISSOMATIC","WAMBAMROLLERMAN", "ROLLYAL WITH CHEESEBOT", "PATCHES O'ROLLIHAN","ROLLBITCH","SNAPCRACKLEMITCHANDROLLBOT","ROLLSLAVE","BIGDADDYROLLS","ROLLROLLROLLYOURBOATBOT","ROLLANDO BLOOM","ROLLTIDE","HITOMATIC","HANDYDANDYROLLBOT","YOUR FRIENDLY NEIGHBORHOOD ROLLBOT","ROLLANDY MARSH", "'THE' ROLLHIO STATE UNIVERSITY","ROLLUMBUSBOT","ROLLGAZO THE MIGHTY ROLL GOD","DROLLPH LUNDGREN","OOO, BAROLLCUDA","ADROLLPH HITLER","RNGESUS","RANDY QUAID","ROLLO TONY BROWN TOWN","ROLLNADOBOT","ROLLNADOBOT II: STILL ROLLING","ROLLNADOBOT III: SUMMER OF ROLLNADOBOT","ROLLNADOBOT IV: ROLL OUT","ROLLNADOBOT V: EASY COME EASY ROLL","ROLLNADOBOT VI: THE FINAL ROLLDOWN","ROLLNADOBOT VII: ONE MORE FOR THE ROLL","ROLLNADOBOT VIII: THE ROLLUNION","ROLLNADOBOT IX: THE PERFECT ROLL","ROLLSY O'DONNELL","TROLLROLLOLBOT","ROLLTANA","CAROLLS SANTANA","CPT SISKROLLS","USS ENTROLLPRISEBOT","DEEP ROLLS 9","RANDOM NUMBER GENERATOR BOT","BEEPBOOPHERESYOURROLL","KING GADROLLA","MECHAGODZIROLLABOT","GODZILLROLLABOT","D-BOT", "THE GREAT ROLLBANZO","VINCENT VAN ROLLBOT","CRAPPYROLLBOT", "BARCEROLLA FC BOT","ROLLERDISCOBOT","ROLLSEPH STALIN","TEDDY ROLLSEVELT","FRANKLIN D. ROLLSEVELT","WINSTROLL CHURCHILL","BENITROLL MUSSOLINI","HIDEKI TOROLL","ROLLEAL MADRIDBOT"]
   var randomCrit = critArray[Math.floor(Math.random()*critArray.length)];
@@ -31,63 +31,53 @@ module.exports = function (req, res, next) {
 
   if (req.body.text) {
     // parse roll type if specified
-	defaultmod = req.body.text.match(/^$|\s*(\+|\-)(\d{1,3})$/);
-    matches = req.body.text.match(/^(\d{1,3})d(\d{1,3})($|\s*(\+|\-)(\d{1,3})$)/);
-	weapons = req.body.text.match(/((\d{1,3})\s*)?(club|dagger|greatclub|handaxe|javelin|lighthammer|mace|quarterstaff|sickle|spear|lightcrossbow|dart|shortbow|sling|battleaxe|flail|glaive|greataxe|greatsword|halberd|lance|longsword|maul|morningstar|pike|rapier|scimitar|shortsword|trident|warpick|warhammer|whip|handcrossbow|heavycrossbow|longbow)($|\s*(\+|\-)(\d{1,3})$)/);
+	matches = req.body.text.match(/((\d{1,3})d(\d{1,3}))?((\d{1,3})\s)?(club|dagger|greatclub|handaxe|javelin|lighthammer|mace|quarterstaff|sickle|spear|lightcrossbow|dart|shortbow|sling|battleaxe|flail|glaive|greataxe|greatsword|halberd|lance|longsword|maul|morningstar|pike|rapier|scimitar|shortsword|trident|warpick|warhammer|whip|handcrossbow|heavycrossbow|longbow)?((\s?)(\+|\-)(\d{1,3}))?((?:\s*[a-zA-Z]+){1,3})?/);
     //matches = req.body.text.match(/^(\d{1,2})d(\d{1,2})$/);
     console.log(matches);
-    if (matches && matches[1] && matches[2]) {
-      times = matches[1];
-      die = matches[2];
-      if (matches[3]){
-        modifier = matches[4];
-        modifier_value = Number(matches[5]);
-        console.log(matches[4]);
-        console.log(matches[5]);
-      }
-
-    } 
-	else if(defaultmod && !weapons) {
+console.log(matches[0]);
+	if (matches) {
 		times = 1;
-		die = 20;
-		modifier = defaultmod[1];
-		modifier_value = Number(defaultmod[2]);
-	}
-	else if(weapons) {
-		times = 1;
-		console.log(weapons);
-		if(weapons[1] && weapons[2]) {
-			times = weapons[2];
+		if(matches[2]) {
+			times = matches[2];
 		}
-		else {
-			times = 1;
+		if(matches[3]) {
+			die = matches[3];
 		}
-		if(/club|dagger|lighthammer|sickle|dart|sling|whip/.test(weapons[3])) {
-			die = 4;
+		if(matches[4] && matches[5]) {
+			times = matches[5];
 		}
-		if(/handaxe|javelin|mace|quarterstaff|spear|shortbow|scimitar|shortsword|trident|handcrossbow|greatsword|maul/.test(weapons[3])) {
-			if(weapons[3] == "greatsword" || weapons[3] == "maul") {
-				times = times*2;
+		if(matches[6]) {
+			if(/club|dagger|lighthammer|sickle|dart|sling|whip/.test(matches[6])) {
+				die = 4;
 			}
-			die = 6;
+			if(/handaxe|javelin|mace|quarterstaff|spear|shortbow|scimitar|shortsword|trident|handcrossbow|greatsword|maul/.test(matches[6])) {
+				if(matches[6] == "greatsword" || matches[6] == "maul") {
+					times = times*2;
+				}
+				die = 6;
+			}
+			if(/greatclub|lightcrossbow|battleaxe|flail|longsword|morningstar|rapier|warpick|warhammer|longbow/.test(matches[6])) {
+				die = 8;
+			}
+			if(/glaive|halberd|pike|heavycrossbow/.test(matches[6])) {
+				die = 10;
+			}
+			if(/greataxe|lance/.test(matches[6])) {
+				die = 12;
+			}
 		}
-		if(/greatclub|lightcrossbow|battleaxe|flail|longsword|morningstar|rapier|warpick|warhammer|longbow/.test(weapons[3])) {
-			die = 8;
+      	if (matches[7]){
+        	modifier = matches[9];
+        	modifier_value = Number(matches[10]);
+      	}
+		if (matches[11]){
+			matches[11].split(' ').join('');
+			rollNote = "(*"+matches[11]+"*)";
 		}
-		if(/glaive|halberd|pike|heavycrossbow/.test(weapons[3])) {
-			die = 10;
-		}
-		if(/greataxe|lance/.test(weapons[3])) {
-			die = 12;
-		}
-		if(weapons[4]) {
-			modifier = weapons[5];
-			modifier_value = Number(weapons[6]*times);
-		}
-	}
+    }
 	else {
       // send error message back to user if input is bad
-      return res.status(200).send('<number>d<sides>');
+      return res.status(200).send('DICE METHOD: <number>d<sides> AND/OR <+/-modifer> AND/OR <3wordmessage> WEAPON METHOD <numberofweaponsifmorethanone> <weaponname> AND/OR <+/-modifer> AND/OR <threewordmessage>');
     }
   }
 
@@ -126,7 +116,7 @@ module.exports = function (req, res, next) {
       console.log(total)
     }
 
-    botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' +
+    botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + rollNote + ':\n' +
                       rolls.join(' + ') + ' (' + modifier + modifier_value + ') = *' + total + '*' + message;
   } 
   else {
@@ -141,7 +131,7 @@ module.exports = function (req, res, next) {
 	else {
 		var message = ""
 	}
-    botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' +
+    botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + rollNote + ':\n' +
                       rolls.join(' + ') + ' = *' + total + '*' + message;
   }
   botPayload.username = randomName;
