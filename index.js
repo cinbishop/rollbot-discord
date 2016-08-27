2506c0332612bc8cec59e40fb338cffc576174cc
Fox <Fox@Jacobs-MBP.ZoomTown.com>
2016-08-27 00:14:49 -0400

    test

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 	beginning with "ping" is sent, it will reply with
 	"pong!".
 */
-
 var Discord = require("discord.js");
 var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.listen(port, function () {
-  console.log('rollbot listening on port ' + port);
+app.listen(port, function() {
+    console.log('rollbot listening on port ' + port);
 });
 
 // Get the email and password
@@ -21,177 +20,164 @@ var bot = new Discord.Client();
 
 //when the bot is ready
 bot.on("ready", () => {
-	console.log(`Ready to begin! Serving in ${bot.channels.length} channels`);
+    console.log(`Ready to begin! Serving in ${bot.channels.length} channels`);
 });
 
 //when the bot disconnects
 bot.on("disconnected", () => {
-	//alert the console
-	console.log("Disconnected!");
+    //alert the console
+    console.log("Disconnected!");
 
-	//exit node.js with an error
-	process.exit(1);
+    //exit node.js with an error
+    process.exit(1);
 });
 
 //when the bot receives a message
 bot.on("message", msg => {
-	
-	var matches = "";
-	var weapons;
-	var rollNote = "";
-	var times = 1;
-	var die = 20;
-	var modifier = "+";
-	var modifier_value = 0;
-	var rolls = [];
-	var total = 0;
-	var botPayload = {};
-	var missEmojiArray = [":hatched_chick:",":poop:",":baby_chick:",":laughing:",":frowning:",":thumbsdown:"];
-	var hitEmojiArray = [":bangbang:",":clap:",":rage:",":hammer:",":bomb:",":skull:"];
-	var critArray = [" CRIT!"," AWH YEAH BIG CRITS!"," CRITTY DITTY DO!"," MMM SEXY CRIT TIMES!"," CRITATTACK!"," M-M-M-MONSTER KILL!", " SUCH CRIT. MUCH DAMAGE.", " GOING... GOING... GONE!", " YAY BIG NUMBERS!", " NICE CRIT, SEXY.", " YOU DONE GOOD, KID", " CRITALCULAR!", " DOINK!"," NICE ONE, BRUVA!"]
-	var missArray = [" YOU SUCK B!"," YOU JUST HIT YOURSELF!"," SLICE! THERE GOES YA PENIS!"," CRITICAL MISS, NUBCAKES!"," WIGGIDTY WAM WAM WOZZLE YA MISSED!"," BABBY'S FIRST SWORD SWING!" ," IS YOUR NAME STEVE FISHER? MAN YOU SUCK.", " LOLOLOLOLOLOL REZ INCOMING NUB", " THAT'S RARELY GOOD", " YA DONE GOOFED", " BIFFED IT", " BIFFED IT HARD", " WELL AT LEAST YOUR PARENTS STILL LOVE YOU", " THIS IS WHY WE CAN'T HAVE NICE THINGS", " I CAN'T BELIEVE YOU'VE DONE THIS", " GOOD JOB, [BLIND CELEBRITY NAME HERE]"]
-	var nameArray = ["ZIMZAMTHEROLLYMAN","ROLLBOT","TRANSFORMANDROLLOUTBOT","BOLLROT","ROLLYPOLLYOLLYROLLBOT","ROLLSMAN5000","CRITOMATIC","MISSOMATIC","WAMBAMROLLERMAN", "ROLLYAL WITH CHEESEBOT", "PATCHES O'ROLLIHAN","ROLLBITCH","SNAPCRACKLEMITCHANDROLLBOT","ROLLSLAVE","BIGDADDYROLLS","ROLLROLLROLLYOURBOATBOT","ROLLANDO BLOOM","ROLLTIDE","HITOMATIC","HANDYDANDYROLLBOT","YOUR FRIENDLY NEIGHBORHOOD ROLLBOT","ROLLANDY MARSH", "'THE' ROLLHIO STATE UNIVERSITY","ROLLUMBUSBOT","ROLLGAZO THE MIGHTY ROLL GOD","DROLLPH LUNDGREN","OOO, BAROLLCUDA","ADROLLPH HITLER","RNGESUS","RANDY QUAID","ROLLO TONY BROWN TOWN","ROLLNADOBOT","ROLLNADOBOT II: STILL ROLLING","ROLLNADOBOT III: SUMMER OF ROLLNADOBOT","ROLLNADOBOT IV: ROLL OUT","ROLLNADOBOT V: EASY COME EASY ROLL","ROLLNADOBOT VI: THE FINAL ROLLDOWN","ROLLNADOBOT VII: ONE MORE FOR THE ROLL","ROLLNADOBOT VIII: THE ROLLUNION","ROLLNADOBOT IX: THE PERFECT ROLL","ROLLSY O'DONNELL","TROLLROLLOLBOT","ROLLTANA","CAROLLS SANTANA","CPT SISKROLLS","USS ENTROLLPRISEBOT","DEEP ROLLS 9","RANDOM NUMBER GENERATOR BOT","BEEPBOOPHERESYOURROLL","KING GADROLLA","MECHAGODZIROLLABOT","GODZILLROLLABOT","D-BOT", "THE GREAT ROLLBANZO","VINCENT VAN ROLLBOT","CRAPPYROLLBOT", "BARCEROLLA FC BOT","ROLLERDISCOBOT","ROLLSEPH STALIN","TEDDY ROLLSEVELT","FRANKLIN D. ROLLSEVELT","WINSTROLL CHURCHILL","BENITROLL MUSSOLINI","HIDEKI TOROLL","ROLLEAL MADRIDBOT" ,"RPBOTSUCKSBOT","RPBOTISNTSOBADBOT","RPBOTHASALOTOFGOODQUALITIESBOT","RPBOTISADICKBOT","RPBOT&ROLLBOT=BFF4EBOT"]
-	var randomCrit = critArray[Math.floor(Math.random()*critArray.length)];
-	var randomMiss = missArray[Math.floor(Math.random()*missArray.length)];
-	var randomName = nameArray[Math.floor(Math.random()*nameArray.length)];
-	var randomMissEmoji = missEmojiArray[Math.floor(Math.random()*missEmojiArray.length)];
-    var randomHitEmoji = hitEmojiArray[Math.floor(Math.random()*hitEmojiArray.length)];
- 
 
-	var didCrit = false;
-	var didMiss = false;
-	
-	var prefix = "/";
-	
-	if(!msg.content.startsWith(prefix)) return;
-	
-		
-	if(msg.content.startsWith(prefix) && msg.content.match(/\/roll$/)) {
-		matches = msg.content.match(/\/roll$/);
-	}
-	
-	else if(msg.content.startsWith(prefix) && msg.content.match(/\/roll\s((\d{1,3})d(\d{1,3}))?((\d{1,3})(\s)?)?(club|dagger|greatclub|handaxe|javelin|lighthammer|mace|quarterstaff|sickle|spear|lightcrossbow|dart|shortbow|sling|battleaxe|flail|glaive|greataxe|greatsword|halberd|lance|longsword|maul|morningstar|pike|rapier|scimitar|shortsword|trident|warpick|warhammer|whip|handcrossbow|heavycrossbow|longbow)?((\s?)(\+|\-)(\d{1,3}))?((\s)?\-(.*$))?/)) {
-		matches = msg.content.match(/\/roll\s((\d{1,3})d(\d{1,3}))?((\d{1,3})(\s)?)?(club|dagger|greatclub|handaxe|javelin|lighthammer|mace|quarterstaff|sickle|spear|lightcrossbow|dart|shortbow|sling|battleaxe|flail|glaive|greataxe|greatsword|halberd|lance|longsword|maul|morningstar|pike|rapier|scimitar|shortsword|trident|warpick|warhammer|whip|handcrossbow|heavycrossbow|longbow)?((\s?)(\+|\-)(\d{1,3}))?((\s)?\-(.*$))?/);
-		    console.log(matches);
-		console.log(matches[0]);
-			if (matches) {
-				times = 1;
-				if(matches[2]) {
-					times = matches[2];
-				}
-				if(matches[3]) {
-					die = matches[3];
-				}
-				if(matches[4] && matches[5]) {
-					times = matches[5];
-				}
-				if(matches[7]) {
-					if(/club|dagger|lighthammer|sickle|dart|sling|whip/.test(matches[7])) {
-						die = 4;
-					}
-					if(/handaxe|javelin|mace|quarterstaff|spear|shortbow|scimitar|shortsword|trident|handcrossbow|greatsword|maul/.test(matches[7])) {
-						if(matches[7] == "greatsword" || matches[7] == "maul") {
-							times = times*2;
-						}
-						die = 6;
-					}
-					if(/greatclub|lightcrossbow|battleaxe|flail|longsword|morningstar|rapier|warpick|warhammer|longbow/.test(matches[7])) {
-						die = 8;
-					}
-					if(/glaive|halberd|pike|heavycrossbow/.test(matches[7])) {
-						die = 10;
-					}
-					if(/greataxe|lance/.test(matches[7])) {
-						die = 12;
-					}
-				}
-		      	if (matches[8]){
-		        	modifier = matches[10];
-		        	modifier_value = Number(matches[11]);
-		      	}
-				if (matches[12]){
-					if(matches[14] === "help") {
-						      bot.sendMessage(msg, "DICE METHOD: <number>d<sides> AND/OR <+/-modifer> AND/OR <-message> \n WEAPON METHOD: <numberofweaponsifmorethanone> <weaponname> AND/OR <+/-modifer> AND/OR <-message> \n Use message -dm for private roll");
-					}
-					else {
-					rollNote = "(**"+matches[14]+"**)";
-					}
-				}
-		    }
-		
-		//send a message to the channel the ping message was sent in.
-		//bot.sendMessage(msg, "pong!");
+    var matches = "";
+    var weapons;
+    var rollNote = "";
+    var times = 1;
+    var die = 20;
+    var modifier = "+";
+    var modifier_value = 0;
+    var rolls = [];
+    var total = 0;
+    var botPayload = {};
+    var missEmojiArray = [":hatched_chick:", ":poop:", ":baby_chick:", ":laughing:", ":frowning:", ":thumbsdown:"];
+    var hitEmojiArray = [":bangbang:", ":clap:", ":rage:", ":hammer:", ":bomb:", ":skull:"];
+    var critArray = [" CRIT!", " AWH YEAH BIG CRITS!", " CRITTY DITTY DO!", " MMM SEXY CRIT TIMES!", " CRITATTACK!", " M-M-M-MONSTER KILL!", " SUCH CRIT. MUCH DAMAGE.", " GOING... GOING... GONE!", " YAY BIG NUMBERS!", " NICE CRIT, SEXY.", " YOU DONE GOOD, KID", " CRITALCULAR!", " DOINK!", " NICE ONE, BRUVA!"]
+    var missArray = [" YOU SUCK B!", " YOU JUST HIT YOURSELF!", " SLICE! THERE GOES YA PENIS!", " CRITICAL MISS, NUBCAKES!", " WIGGIDTY WAM WAM WOZZLE YA MISSED!", " BABBY'S FIRST SWORD SWING!", " IS YOUR NAME STEVE FISHER? MAN YOU SUCK.", " LOLOLOLOLOLOL REZ INCOMING NUB", " THAT'S RARELY GOOD", " YA DONE GOOFED", " BIFFED IT", " BIFFED IT HARD", " WELL AT LEAST YOUR PARENTS STILL LOVE YOU", " THIS IS WHY WE CAN'T HAVE NICE THINGS", " I CAN'T BELIEVE YOU'VE DONE THIS", " GOOD JOB, [BLIND CELEBRITY NAME HERE]"]
+    var nameArray = ["ZIMZAMTHEROLLYMAN", "ROLLBOT", "TRANSFORMANDROLLOUTBOT", "BOLLROT", "ROLLYPOLLYOLLYROLLBOT", "ROLLSMAN5000", "CRITOMATIC", "MISSOMATIC", "WAMBAMROLLERMAN", "ROLLYAL WITH CHEESEBOT", "PATCHES O'ROLLIHAN", "ROLLBITCH", "SNAPCRACKLEMITCHANDROLLBOT", "ROLLSLAVE", "BIGDADDYROLLS", "ROLLROLLROLLYOURBOATBOT", "ROLLANDO BLOOM", "ROLLTIDE", "HITOMATIC", "HANDYDANDYROLLBOT", "YOUR FRIENDLY NEIGHBORHOOD ROLLBOT", "ROLLANDY MARSH", "'THE' ROLLHIO STATE UNIVERSITY", "ROLLUMBUSBOT", "ROLLGAZO THE MIGHTY ROLL GOD", "DROLLPH LUNDGREN", "OOO, BAROLLCUDA", "ADROLLPH HITLER", "RNGESUS", "RANDY QUAID", "ROLLO TONY BROWN TOWN", "ROLLNADOBOT", "ROLLNADOBOT II: STILL ROLLING", "ROLLNADOBOT III: SUMMER OF ROLLNADOBOT", "ROLLNADOBOT IV: ROLL OUT", "ROLLNADOBOT V: EASY COME EASY ROLL", "ROLLNADOBOT VI: THE FINAL ROLLDOWN", "ROLLNADOBOT VII: ONE MORE FOR THE ROLL", "ROLLNADOBOT VIII: THE ROLLUNION", "ROLLNADOBOT IX: THE PERFECT ROLL", "ROLLSY O'DONNELL", "TROLLROLLOLBOT", "ROLLTANA", "CAROLLS SANTANA", "CPT SISKROLLS", "USS ENTROLLPRISEBOT", "DEEP ROLLS 9", "RANDOM NUMBER GENERATOR BOT", "BEEPBOOPHERESYOURROLL", "KING GADROLLA", "MECHAGODZIROLLABOT", "GODZILLROLLABOT", "D-BOT", "THE GREAT ROLLBANZO", "VINCENT VAN ROLLBOT", "CRAPPYROLLBOT", "BARCEROLLA FC BOT", "ROLLERDISCOBOT", "ROLLSEPH STALIN", "TEDDY ROLLSEVELT", "FRANKLIN D. ROLLSEVELT", "WINSTROLL CHURCHILL", "BENITROLL MUSSOLINI", "HIDEKI TOROLL", "ROLLEAL MADRIDBOT", "RPBOTSUCKSBOT", "RPBOTISNTSOBADBOT", "RPBOTHASALOTOFGOODQUALITIESBOT", "RPBOTISADICKBOT", "RPBOT&ROLLBOT=BFF4EBOT"]
+    var randomCrit = critArray[Math.floor(Math.random() * critArray.length)];
+    var randomMiss = missArray[Math.floor(Math.random() * missArray.length)];
+    var randomName = nameArray[Math.floor(Math.random() * nameArray.length)];
+    var randomMissEmoji = missEmojiArray[Math.floor(Math.random() * missEmojiArray.length)];
+    var randomHitEmoji = hitEmojiArray[Math.floor(Math.random() * hitEmojiArray.length)];
 
-		//alert the console
-		//console.log("pong-ed " + msg.author.username);
-	}
 
-	if(matches) {
-	// roll dice and sum
-  for (var i = 0; i < times; i++) {
-    var currentRoll = roll(1, die);
-	var rollTotal = times*die;
-	var badRoll = times*1;
-    rolls.push(currentRoll);
-    total += currentRoll;
-  }
-	
-	if (modifier_value){
-	var unmodifiedTotal = total;
+    var didCrit = false;
+    var didMiss = false;
 
-	if(unmodifiedTotal === rollTotal) {
-		var message = randomCrit;
-		didCrit = true;
-	}
-	else if(unmodifiedTotal === badRoll) {
-		var message = randomMiss;
-		didMiss = true;
-	}
-	else {
-		var message = ""
-	}
+    var prefix = "/";
 
-    if (modifier == '+'){
-      total = total + modifier_value;
-      console.log(total)
+    if (!msg.content.startsWith(prefix)) return;
+
+
+    if (msg.content.startsWith(prefix) && msg.content.match(/\/roll$/)) {
+        matches = msg.content.match(/\/roll$/);
+    } else if (msg.content.startsWith(prefix) && msg.content.match(/\/roll\s((\d{1,3})d(\d{1,3}))?((\d{1,3})(\s)?)?(club|dagger|greatclub|handaxe|javelin|lighthammer|mace|quarterstaff|sickle|spear|lightcrossbow|dart|shortbow|sling|battleaxe|flail|glaive|greataxe|greatsword|halberd|lance|longsword|maul|morningstar|pike|rapier|scimitar|shortsword|trident|warpick|warhammer|whip|handcrossbow|heavycrossbow|longbow)?((\s?)(\+|\-)(\d{1,3}))?((\s)?\-(.*$))?/)) {
+        matches = msg.content.match(/\/roll\s((\d{1,3})d(\d{1,3}))?((\d{1,3})(\s)?)?(club|dagger|greatclub|handaxe|javelin|lighthammer|mace|quarterstaff|sickle|spear|lightcrossbow|dart|shortbow|sling|battleaxe|flail|glaive|greataxe|greatsword|halberd|lance|longsword|maul|morningstar|pike|rapier|scimitar|shortsword|trident|warpick|warhammer|whip|handcrossbow|heavycrossbow|longbow)?((\s?)(\+|\-)(\d{1,3}))?((\s)?\-(.*$))?/);
+        console.log(matches);
+        console.log(matches[0]);
+        if (matches) {
+            times = 1;
+            if (matches[2]) {
+                times = matches[2];
+            }
+            if (matches[3]) {
+                die = matches[3];
+            }
+            if (matches[4] && matches[5]) {
+                times = matches[5];
+            }
+            if (matches[7]) {
+                if (/club|dagger|lighthammer|sickle|dart|sling|whip/.test(matches[7])) {
+                    die = 4;
+                }
+                if (/handaxe|javelin|mace|quarterstaff|spear|shortbow|scimitar|shortsword|trident|handcrossbow|greatsword|maul/.test(matches[7])) {
+                    if (matches[7] == "greatsword" || matches[7] == "maul") {
+                        times = times * 2;
+                    }
+                    die = 6;
+                }
+                if (/greatclub|lightcrossbow|battleaxe|flail|longsword|morningstar|rapier|warpick|warhammer|longbow/.test(matches[7])) {
+                    die = 8;
+                }
+                if (/glaive|halberd|pike|heavycrossbow/.test(matches[7])) {
+                    die = 10;
+                }
+                if (/greataxe|lance/.test(matches[7])) {
+                    die = 12;
+                }
+            }
+            if (matches[8]) {
+                modifier = matches[10];
+                modifier_value = Number(matches[11]);
+            }
+            if (matches[12]) {
+                if (matches[14] === "help") {
+                    bot.sendMessage(msg, "DICE METHOD: <number>d<sides> AND/OR <+/-modifer> AND/OR <-message> \n WEAPON METHOD: <numberofweaponsifmorethanone> <weaponname> AND/OR <+/-modifer> AND/OR <-message> \n Use message -dm for private roll");
+                } else {
+                    rollNote = "(**" + matches[14] + "**)";
+                }
+            }
+        }
+
+        //send a message to the channel the ping message was sent in.
+        //bot.sendMessage(msg, "pong!");
+
+        //alert the console
+        //console.log("pong-ed " + msg.author.username);
     }
 
-    else if(modifier == '-'){
-      total = total - modifier_value;
-      console.log(total)
+    if (matches) {
+        // roll dice and sum
+        for (var i = 0; i < times; i++) {
+            var currentRoll = roll(1, die);
+            var rollTotal = times * die;
+            var badRoll = times * 1;
+            rolls.push(currentRoll);
+            total += currentRoll;
+        }
+
+        if (modifier_value) {
+            var unmodifiedTotal = total;
+
+            if (unmodifiedTotal === rollTotal) {
+                var message = randomCrit;
+                didCrit = true;
+            } else if (unmodifiedTotal === badRoll) {
+                var message = randomMiss;
+                didMiss = true;
+            } else {
+                var message = ""
+            }
+
+            if (modifier == '+') {
+                total = total + modifier_value;
+                console.log(total)
+            } else if (modifier == '-') {
+                total = total - modifier_value;
+                console.log(total)
+            }
+            //msg.channel.server.detailsOf(msg.author).nick
+            botPayload.text = 'you rolled ' + times + 'd' + die + rollNote + ':\n' +
+                rolls.join(' + ') + ' (' + modifier + modifier_value + ') = **' + total + ' ' + message + '**';
+        } else {
+            if (total === rollTotal) {
+                var message = randomCrit;
+                didCrit = true;
+            } else if (total === badRoll) {
+                var message = randomMiss;
+                didMiss = true;
+            } else {
+                var message = ""
+            }
+            botPayload.text = 'you rolled ' + times + 'd' + die + rollNote + ':\n' +
+                rolls.join(' + ') + ' = **' + total + ' ' + message + '**';
+        }
+        if (didCrit) {} else if (didMiss) {}
+        botPayload.username = randomName;
+        bot.setNickname(msg, botPayload.username);
+        bot.reply(msg, botPayload.text);
+        bot.deleteMessage(msg);
     }
-    //msg.channel.server.detailsOf(msg.author).nick
-    botPayload.text = 'you rolled ' + times + 'd' + die + rollNote + ':\n' +
-                      rolls.join(' + ') + ' (' + modifier + modifier_value + ') = **' + total + ' ' + message + '**';
-  } 
-  else {
-	if(total === rollTotal) {
-		var message = randomCrit;
-		didCrit = true;
-	}
-	else if(total === badRoll) {
-		var message = randomMiss;
-		didMiss = true;
-	}
-	else {
-		var message = ""
-	}
-    botPayload.text = 'you rolled ' + times + 'd' + die + rollNote + ':\n' +
-                      rolls.join(' + ') + ' = **' + total + ' ' + message + '**';
-  }
-   if(didCrit) {
-   }
-   else if(didMiss) {
-   }
-   botPayload.username = randomName;
-   bot.setNickname(msg, botPayload.username);
-   bot.reply(msg, botPayload.text);
-   bot.deleteMessage(msg);
-}
 });
 
-function roll (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function roll(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 bot.loginWithToken(AuthDetails.token);
