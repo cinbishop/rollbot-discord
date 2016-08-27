6e23729bcbe365e947b9739f5a48d6251559ae76
Fox <Fox@Jacobs-MBP.ZoomTown.com>
2016-08-27 12:26:40 -0400

    test

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on("ready", () => {
     console.log(`Ready to begin! Serving in ${bot.channels.length} channels`);
     genChannel = bot.channels.get("name","dm_rolls").id;
     //1440000
-    idleTimer = setInterval(keepAwake, 5000);
+    idleTimer = setInterval(keepAwake, 1440000);
 });
 
 //when the bot disconnects
@@ -185,7 +185,7 @@ bot.on("message", msg => {
         bot.deleteMessage(msg);
         clearInterval(idleTimer)
         //1440000
-        idleTimer = setInterval(keepAwake, 5000);
+        idleTimer = setInterval(keepAwake, 1440000);
     }
 });
 
