afb5be55a5bc7914134a678dff61792065a7abbc
Fox <Fox@Jacobs-MBP.ZoomTown.com>
2016-08-27 01:10:57 -0400

    test

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
 app.listen(port, function() {
     console.log('rollbot listening on port ' + port);
 });
